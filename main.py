62690be6ba267ae4874e4960719310c75f811624
Tulezhanov <n.tulezhanov92@gmail.com>
2023-10-22 19:23:50 +0500

    main

---
+++ main.py
@@ -0,0 +1,23 @@
+first_name = input("first name:")
+last_name = input("last name:")
+year_of_birth = input("year of birth:")
+do_you_like_of_course = input("do you like of course?")
+what_do_you_expect_in_future_courses = input("what do you expect in future courses?")
+
+print("full name:", first_name, last_name)
+print ("age:", 2023 - int(year_of_birth))
+print("your answer to the first question:", str(do_you_like_of_course))
+print("your answer to the second question:", str(what_do_you_expect_in_future_courses))
+
+
+
+
+
+
+
+
+
+
+
+
+
